f2c4597e118ac325855b6a9239aedf7e9926c657
Jonathan Frederick <jrfreder@gmail.com>
2015-10-26 21:23:18 -0400

    generating activities

--- fitter_api/api/management/commands/generate_data.py
+++ fitter_api/api/management/commands/generate_data.py
@@ -1,5 +1,6 @@
 from ...models import *
 from faker import Faker
+import random
 from django.contrib.auth.models import User
 
 from django.core.management.base import BaseCommand
@@ -11,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         create_users()
-
+        create_activities()
 
 def create_users():
     for _ in range(10):
@@ -19,3 +20,11 @@ def create_users():
                                             password='password',
                                             email='')
         # new_user.save()
+
+
+def create_activities():
+    for _ in range(50):
+        new_activity = Activity(title=fake.bs(),
+                                units=fake.text(max_nb_chars=9),
+                                user=random.choice(User.objects.all()))
+        # new_activity.save()
