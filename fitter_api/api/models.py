75fd090a3dc21157351d8f5c3526b19d2a2cc892
Karthik <karthik.kasala@gmail.com>
2015-10-28 13:14:30 -0400

    fixed corsheaders middleware typo

--- fitter_api/api/models.py
+++ fitter_api/api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Activity(models.Model):
     title = models.CharField(max_length=50)
     units = models.CharField(max_length=10)
